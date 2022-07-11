63f91ed5a89da10775f55d25458b0b5ec317d423
Manaswini <manaswinibk@outlook.com>
2022-07-11 22:45:44 +0530

    Adding all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'credit-card';
+  ccForm = new FormGroup({
+    nameOnCard : new FormControl('',[Validators.required, Validators.minLength(3)]),
+    cardNum: new FormControl('',[Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
+    expiration: new FormControl('',[Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    cvv : new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(3) ])
+  })
+
+  get nameOnCard(){
+    return (<FormControl>this.ccForm.get('nameOnCard'));
+  }
+
+  get cardNum(){
+    return (<FormControl>this.ccForm.get('cardNum'));
+  }
+
+  get expiration(){
+    return (<FormControl>this.ccForm.get('expiration'));
+  }
+
+  get cvv(){
+    return (<FormControl>this.ccForm.get('cvv'));
+  }
+
+  submit(){
+    console.log("Form submitted")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from './input/input.component';
+import { NgxMaskModule} from 'ngx-mask';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
